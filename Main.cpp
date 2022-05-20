52f10db76061afbb1ce5cef989256ab3a04d3673
Wasif Johar <77242506+Wjoe21@users.noreply.github.com>
2022-05-20 12:43:14 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -7,40 +7,13 @@ using namespace std;
 
 int main(){
 
-  //string fileName;
-  //int chosenOption;
-  CipherAdvanced cipher;
-
-  /* cout << "Press 1 to encrypt or 2 to decrypt: ";
-  cin >> chosenOption;
-  
-  switch(chosenOption){
-  case 1:
-    cout << "please enter file name to encrypt: ";
-    cin >> fileName;
-    cipher.encodeFile(fileName);
-    break;
-  case 2:
-    cout << "please enter file name to decrypt: ";
-    cin >> fileName;
-    cipher.decodeFile(fileName);
-    break;
-  default:
-    break;
-    };*/
-
-  cipher.unknownScramble("plaue");
-
-  //Cipher cipherObj;
-  //int * ptr = cipherObj.getCipher();
-  //cout << *(ptr + 0) << " " << *(ptr + 1) << " " << *(ptr + 2) << " " << *(ptr + 3) << endl;*/
+  Cipher cipherObj;
  
-  //int key[3] = {1, 0, 2};
-  //cipherObj.setCipher(3, key);
-  //int * newPtr = cipherObj.getCipher();
-  //cout << newPtr << endl;
-  //string cipherStr = cipherObj.encodeMessage("south");
-  //cout << cipherStr << endl;
-  //string decodeStr = cipherObj.decodeMessage("Igopp vkmt kv e ugvxQnhetg hrdtbtu oh xicqo zqx");
-  //cout << decodeStr << endl;
+  int key[3] = {1, 0, 2};
+  cipherObj.setCipher(3, key);
+  
+  string cipherStr = cipherObj.encodeMessage("south");
+  cout << cipherStr << endl;
+  string decodeStr = cipherObj.decodeMessage("Igopp vkmt kv e ugvxQnhetg hrdtbtu oh xicqo zqx");
+  cout << decodeStr << endl;
 }

7bcbbeb6a985409304ec516141b782f7ff33c6f8
Wasif Johar <77242506+Wjoe21@users.noreply.github.com>
2022-05-20 12:41:46 -0400

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,9 +1,4 @@
 /*
-  Wasif Johar
-  CPSC 1071: 003
-  C++// makes C bigger
-  4/8/22
-
   This is the header file for class Queue. It has a struct QueueNode with a constructor that creates
   a new QueueNode. There are 3 pointers, next, front, and rear. The class Queue has one private variable
   that keeps track of the ongoing amount of current nodes in the queue, and 7 public methods
